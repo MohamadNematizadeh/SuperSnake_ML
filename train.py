8a0385c9a4901598f83c77e50ebfe2de60b39826
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2023-09-17 15:13:56 +0330

    Update files

--- train.py
+++ train.py
@@ -6,7 +6,6 @@ data = pd.read_csv('dataset/Super_Snake.csv')
 data.fillna(0, inplace=True)
 X = data.iloc[:, :-1].values 
 Y = data.iloc[:, -1].values  
-learning_rate =0.002
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(12, activation='relu'),
@@ -14,7 +13,11 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(30, activation='relu'),
     tf.keras.layers.Dense(4, activation='softmax')
 ])
-model.compile(optimizer='adam',
+# 80/80 [==============================] - 0s 1ms/step - loss: 0.0222 - accuracy: 0.9909
+# 20/20 [==============================] - 0s 1ms/step - loss: 0.0110 - accuracy: 0.9953
+# loss test: 0.011022300459444523
+# accuracy test: 0.9952830076217651
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.002),
               loss= tf.keras.losses.sparse_categorical_crossentropy,
               metrics=['accuracy'])
 output = model.fit(X_train, Y_train, epochs=200)
